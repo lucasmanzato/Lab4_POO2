1ae6c2e44254539f059789d25d98fbf8a62b7046
lucas manzato <lucas.manzato@hotmail.com>
2024-02-04 19:46:44 -0300

    Dois ex lab 2

---
+++ OneDrive/Documentos/NetBeansProjects/lab2/src/main/java/com/mycompany/lab2/Celular.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab2;
+
+/**
+ *
+ * @author lucas
+ */
+public interface Celular {
+    
+    public String getModelo();
+    
+    public void fazLigacao();
+    
+    public void tiraFoto();
+}
